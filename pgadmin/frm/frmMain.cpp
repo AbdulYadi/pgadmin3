77c48f3aa2cd276b752b3024635502bc816ae6fb
Abdul Yadi <abdulyadi.datatrans@gmail.com>
2020-02-27 15:33:48 +0700

    push context menu [Delete/Drop] away from [Disconnect database] as suggested by Sudirman

--- pgadmin/frm/frmMain.cpp
+++ pgadmin/frm/frmMain.cpp
@@ -381,8 +381,10 @@ void frmMain::CreateMenus()
 	new addnamedrestorepointServiceFactory(menuFactories, toolsMenu, 0);
 
 	new createFactory(menuFactories, editMenu, toolBar);
-	new dropFactory(menuFactories, editMenu, toolBar);
-	new dropCascadedFactory(menuFactories, editMenu, 0);
+/*ABDUL:BEGIN 27 Feb 2020: push drop menu away from create*/
+	//new dropFactory(menuFactories, editMenu, toolBar);
+	//new dropCascadedFactory(menuFactories, editMenu, 0);
+/*ABDUL:END*/	
 	new truncateFactory(menuFactories, editMenu, 0);
 	new truncateCascadedFactory(menuFactories, editMenu, 0);
 	new resetTableStatsFactory(menuFactories, editMenu, 0);
@@ -390,6 +392,11 @@ void frmMain::CreateMenus()
 	new reassignDropOwnedFactory(menuFactories, editMenu, 0);
 	new moveTablespaceFactory(menuFactories, editMenu, 0);
 	new searchObjectFactory(menuFactories, editMenu, 0);
+/*ABDUL:BEGIN 27 Feb 2020: push drop menu away from create*/
+	new dropFactory(menuFactories, editMenu, toolBar);
+	new dropCascadedFactory(menuFactories, editMenu, 0);	
+/*ABDUL:END*/	
+
 	editMenu->AppendSeparator();
 
 	new separatorFactory(menuFactories);
