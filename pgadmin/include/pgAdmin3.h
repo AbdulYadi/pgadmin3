1feaf211c75686d49aaac988b4e64e198d5a42f7
Abdul Yadi <abdulyadi.datatrans@gmail.com>
2020-05-20 11:27:26 +0700

    extend to PostgreSQL 12.3

--- pgadmin/include/pgAdmin3.h
+++ pgadmin/include/pgAdmin3.h
@@ -60,8 +60,8 @@
 const short SERVER_MIN_VERSION_N = 0x0804;
 const wxString SERVER_MIN_VERSION_T = wxT("8.4");
 /*ABDUL:BEGIN*/
-const short SERVER_MAX_VERSION_N = 0x0C02;
-const wxString SERVER_MAX_VERSION_T = wxT("12.2");
+const short SERVER_MAX_VERSION_N = 0x0C03;
+const wxString SERVER_MAX_VERSION_T = wxT("12.3");
 /*ABDUL:END*/
 
 // Supported Greenplum Database and Greenplum HAWQ minimum and maximum values.
